2f4a4c3656cee7a1e899bb4451582e475edb560b
Christie Desnoyer <christiedesnoyer@Christies-MacBook-Pro.local>
2019-01-22 14:09:27 -0500

    Adding logic to get Shape Calculator up and running

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,55 @@
+package com.company;
+
+public class Calculator {
+
+
+    private double shapeHeight;
+    private double shapeWidth;
+    private String shape;
+
+
+    public double getShapeHeight() {
+        return shapeHeight;
+    }
+
+    public void setShapeHeight(double shapeHeight) {
+        this.shapeHeight = shapeHeight;
+    }
+
+    public double getShapeWidth() {
+        return shapeWidth;
+    }
+
+    public void setShapeWidth(double shapeWidth) {
+        this.shapeWidth = shapeWidth;
+    }
+
+    public String getShape() {
+        return shape;
+    }
+
+    public void setShape(String shape) {
+        this.shape = shape;
+    }
+
+
+    public void calculateArea(String shape, double shapeHeight, double shapeWidth){
+
+        double area = 0.0;
+
+        if (shape.equalsIgnoreCase("triangle")){
+
+            System.out.println("The area of a triangle is : " + ((shapeHeight * shapeWidth)/2));
+
+        }
+        else if (shape.equalsIgnoreCase("rectangle")){
+
+            System.out.println("The area of a rectangle is : " + (shapeHeight * shapeWidth));
+
+        }
+
+
+
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+      Calculator calc = new Calculator();
+
+      calc.calculateArea("Triangle", 5.5, 6.5);
+      calc.calculateArea("Rectangle", 5, 10);
+
+
+
+
+    }
+}
