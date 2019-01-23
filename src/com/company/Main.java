d2588c2cc7b15580441443bc4cf6ccdf8c3a5460
Christie Desnoyer <christiedesnoyer@Christies-MacBook-Pro.local>
2019-01-22 19:33:39 -0500

    Adding a method to continue if the user so chooses

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,36 +5,64 @@ public class Main {
 
     public static void main(String[] args) {
 
+        boolean userContinue = true;
+
       Calculator calc = new Calculator();
 
       Scanner scan = new Scanner (System.in);
 
 
-      System.out.println("What kind of shape would you like to get the area of? \n Type triangle or rectangle.");
+      do {
 
-      String userShape = scan.nextLine();
+          System.out.println("What kind of shape would you like to get the area of? \n Type triangle or rectangle.");
 
-      if (!userShape.equalsIgnoreCase("triangle") || !userShape.equalsIgnoreCase("rectangle")){
-
-          System.out.println("Not a valid option, enter either TRIANGLE or RECTANGLE");
-          userShape = scan.nextLine();
-      }
-
-      System.out.println("Enter the shape's height");
-      double userHeight = scan.nextDouble();
-
-        System.out.println("Enter the shape's Length");
-        double userLength = scan.nextDouble();
+          String userShape = scan.next();
 
 
-        calc.calculateArea(userShape, userHeight, userLength);
+          System.out.println("Enter the shape's height");
+          double userHeight = scan.nextDouble();
+
+          System.out.println("Enter the shape's Length");
+          double userLength = scan.nextDouble();
+
+
+          calc.calculateArea(userShape, userHeight, userLength);
+
+          System.out.println("Would you like to perform another calculation? Press 1 for yes and 2 for no.");
+
+          int userChoice = scan.nextInt();
+
+
+        userContinue=  isUserContinuing(userChoice);
+
+
+      } while (userContinue);
+
+      System.out.println("Thank you for calculating shapes!");
+
+
 
         // these were just for testing
 //      calc.calculateArea("Triangle", 5.5, 6.5);
 //      calc.calculateArea("Rectangle", 5, 10);
 
-
-
-
     }
+
+    private static boolean isUserContinuing(int userChoice) {
+
+        boolean userContinue;
+
+        if (userChoice == 1) {
+
+            userContinue = true;
+
+
+        } else {
+
+            userContinue = false;
+        }
+        return userContinue;
+    }
+
+
 }
